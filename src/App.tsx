35d36800140c919c8676b2c8a5283c6e96efae19
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-12-17 10:14:10 -0700

    Notes on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //    Destructuring       React Hook
+  const [count, setCount] = useState(0) 
 
   return (
+    //Empty set of angular brackets is a react node (render fragment), it wraps it and displays without an extra div.
     <>
-      <div className="card">
+    {/* className - Because class is a reserved keyword and here you are writing tsx */}
+      <div className="card" aria-atomic="true" >
+        {/* Curly braces { }, and allows you to bind to and or write JS, TS */}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
